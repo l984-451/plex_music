b87be07eb7407e8c6542edf4af0e6310e49a5b9f
Bain Gurley <baingurley@gmail.com>
2024-04-19 12:39:23 -0500

    Initial Commit

---
+++ plex_music Watch App/plex_musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  plex_musicApp.swift
+//  plex_music Watch App
+//
+//  Created by Bain Gurley on 4/19/24.
+//
+
+import SwiftUI
+
+@main
+struct plex_music_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
